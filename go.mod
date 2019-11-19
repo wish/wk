023135db368f2b8d6e1dc7afad75ee59606c4d43
Alex Kursell <akursell@wish.com>
2019-11-18 16:20:12 -0800

    Move from patching to overwriting kops resources. Maintain more state from subcommands

--- go.mod
+++ go.mod
@@ -15,7 +15,6 @@ require (
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/kr/fs v0.1.0 // indirect
 	github.com/kylelemons/godebug v1.1.0
-	github.com/pkg/errors v0.8.1 // indirect
 	github.com/pkg/sftp v1.10.0 // indirect
 	github.com/sirupsen/logrus v1.4.1
 	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337 // indirect
@@ -25,13 +24,13 @@ require (
 	golang.org/x/crypto v0.0.0-20190422183909-d864b10871cd // indirect
 	golang.org/x/net v0.0.0-20190424112056-4829fb13d2c6 // indirect
 	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a // indirect
-	golang.org/x/sys v0.0.0-20190424175732-18eb32c0e2f0 // indirect
 	golang.org/x/text v0.3.1 // indirect
 	google.golang.org/api v0.3.2 // indirect
 	google.golang.org/appengine v1.5.0 // indirect
 	google.golang.org/genproto v0.0.0-20190418145605-e7d98fc518a7 // indirect
 	google.golang.org/grpc v1.20.1 // indirect
 	gopkg.in/ini.v1 v1.46.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2
 	k8s.io/apimachinery v0.0.0-20190424132444-f1e86e15343c // indirect
 	k8s.io/client-go v11.0.0+incompatible // indirect
 	k8s.io/kops v1.11.1-0.20190301151100-0f2aa8d30d89
--- pkg/kops/util.go
+++ pkg/kops/util.go
@@ -1,16 +1,13 @@
 package kops
 
 import (
-	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"reflect"
 	"strings"
 
-	"github.com/imdario/mergo"
-	"github.com/kylelemons/godebug/diff"
+	godebug_diff "github.com/kylelemons/godebug/diff"
 	"github.com/sirupsen/logrus"
-	"sigs.k8s.io/yaml"
 
 	"github.com/wish/wk/pkg/types"
 	"github.com/wish/wk/pkg/util"
@@ -28,48 +25,24 @@ func ReadClusterFile(path string) (*types.Cluster, error) {
 	return cluster, nil
 }
 
-func patch(data []byte, patch map[string]interface{}) (bool, []byte, error) {
-	if bytes.HasPrefix(data, []byte{'#'}) && bytes.Contains(data, []byte("# ...")) {
-		for _, line := range bytes.Split(data, []byte{'\n'}) {
-			if !bytes.HasPrefix(line, []byte{'#'}) {
-				break
-			}
-			logrus.Infoln(line)
-		}
-	}
-	src := &map[string]interface{}{}
-	dst := &map[string]interface{}{}
-	var err error
-
-	if err = yaml.Unmarshal(data, src); err != nil {
-		return false, nil, err
-	}
-	if err = yaml.Unmarshal(data, dst); err != nil {
-		return false, nil, err
-	}
-	if err = mergo.Merge(dst, patch, mergo.WithOverride); err != nil {
-		return false, nil, err
-	}
-
-	if !reflect.DeepEqual(*src, *dst) {
+// diff returns whether the structures are different, and a textual diff of the changes
+func diff(old, new map[string]interface{}) (bool, string) {
+	if !reflect.DeepEqual(old, new) {
 		var A, B []byte
-		if A, err = json.MarshalIndent(src, "", "  "); err != nil {
-			return false, nil, err
+		var err error
+		if A, err = json.MarshalIndent(&old, "", "  "); err != nil {
+			logrus.Fatalf("Error marshalling old to JSON: %v", err)
 		}
-		if B, err = json.MarshalIndent(dst, "", "  "); err != nil {
-			return false, nil, err
+		if B, err = json.MarshalIndent(&new, "", "  "); err != nil {
+			logrus.Fatalf("Error marshalling new to JSON: %v", err)
 		}
-		logrus.Infoln(util.Render(diff.DiffChunks(
+		textDiff := util.Render(godebug_diff.DiffChunks(
 			strings.Split(string(A), "\n"),
 			strings.Split(string(B), "\n"),
-		)))
+		))
 
-		y, err := yaml.Marshal(dst)
-		if err != nil {
-			return false, nil, err
-		}
-		return false, y, nil
+		return false, textDiff
 	}
 
-	return true, nil, nil
+	return true, ""
 }
